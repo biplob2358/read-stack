beef79913f26735a2ed4b1b3afe324508dff55f0
biplob2358 <biplob2358@gmail.com>
2025-07-02 01:27:40 +0600

    remove hard code

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "https://libaray-management-server.vercel.app/api/",
+    baseUrl: import.meta.env.VITE_API_URL,
   }),
   tagTypes: ["Books", "Borrows"],
   endpoints: () => ({}),
